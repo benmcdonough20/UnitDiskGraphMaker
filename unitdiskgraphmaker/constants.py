1dd4bba0152acae052d0c08b31df2f17cc04f3b9
Ben McDonough <benmcdonough20@gmail.com>
2023-02-03 02:06:34 -0500

    reformatted with the black package

--- unitdiskgraphmaker/constants.py
+++ unitdiskgraphmaker/constants.py
@@ -3,7 +3,7 @@ from PyQt6.QtGui import QColorConstants, QColor
 BGCOLOR = QColor("#333333")
 FGCOLOR = QColor("#444444")
 SHADOWCOLOR = QColor("#555555")
-ACCENT1 = QColor("#FFFFFF") #QColor("#FF505D")
+ACCENT1 = QColor("#FFFFFF")  # QColor("#FF505D")
 ACCENT2 = QColor("#6437FF")
 CONNECTION = QColor("#556437FF")
 POTENTIAL = QColor("#FFFFFF")
@@ -13,12 +13,12 @@ LINECOLOR = QColorConstants.Black
 ATOM_SIZE = 30
 WIRE_SIZE = 3
 
-RYDBERG_BLOCKADE = 6.1e-6 #in m
+RYDBERG_BLOCKADE = 6.1e-6  # in m
 SCALE = 60
-MIN_YSPACING = .66
-MIN_SPACING = .66
+MIN_YSPACING = 0.66
+MIN_SPACING = 0.66
 
-MAXHEIGHT = 12.46 #in diskgraph units
+MAXHEIGHT = 12.46  # in diskgraph units
 MAXWIDTH = 12.3
 
 SHADOW_WIDTH = 2
--- unitdiskgraphmaker/gui.py
+++ unitdiskgraphmaker/gui.py
@@ -1,6 +1,6 @@
 from PyQt6.QtWidgets import (
-    QMainWindow, 
-    QToolBar, 
+    QMainWindow,
+    QToolBar,
     QFileDialog,
     QApplication,
     QPushButton,
@@ -16,14 +16,15 @@ from canvas import SmartCanvas
 
 
 class CircuitGui(QMainWindow):
-
     def __init__(self):
         super().__init__()
         self.initUI()
 
     def initUI(self):
         self.setWindowTitle("Unit Disk Graph Maker")
-        self.setMinimumSize(QSize(round(SCALE*(MAXWIDTH+2)), round(SCALE*(MAXHEIGHT+2))))
+        self.setMinimumSize(
+            QSize(round(SCALE * (MAXWIDTH + 2)), round(SCALE * (MAXHEIGHT + 2)))
+        )
 
         self.canvas = SmartCanvas(self)
 
@@ -40,7 +41,7 @@ class CircuitGui(QMainWindow):
         self.toolbar.addAction(open_file)
         open_file.triggered.connect(self.load)
 
-        save = QAction("",self)
+        save = QAction("", self)
         save.setIcon(QIcon("../icons/save.png"))
         self.toolbar.addAction(save)
         save.triggered.connect(self.save)
@@ -54,7 +55,7 @@ class CircuitGui(QMainWindow):
         export.setIcon(QIcon("../icons/export.png"))
         self.toolbar.addAction(export)
         export.triggered.connect(self.export)
-   
+
         togglePotential = QPushButton("", self)
         togglePotential.setIcon(QIcon("../icons/potential.png"))
         togglePotential.setCheckable(True)
@@ -68,49 +69,52 @@ class CircuitGui(QMainWindow):
         toggleLinks.clicked.connect(self.toggleLinks)
 
         self.toolbar.setContextMenuPolicy(Qt.ContextMenuPolicy.PreventContextMenu)
-        self.addToolBar(self.toolbar) 
+        self.addToolBar(self.toolbar)
 
     def save(self):
         fname = self.fchooser("Save File")
         with open(fname, "w") as f:
             for atom in self.canvas.atoms:
-                f.write(str(atom.x)+","+str(atom.y)+"\n")
+                f.write(str(atom.x) + "," + str(atom.y) + "\n")
 
     def export(self):
         fname = self.fchooser("Export Points")
         with open(fname, "w") as f:
             f.write("x,y\n")
             for atom in self.canvas.atoms:
-                f.write(f"{(atom.x-SCALE)/SCALE*RYDBERG_BLOCKADE},{(atom.y-SCALE)/SCALE*RYDBERG_BLOCKADE}\n")
-    
+                f.write(
+                    f"{(atom.x-SCALE)/SCALE*RYDBERG_BLOCKADE},{(atom.y-SCALE)/SCALE*RYDBERG_BLOCKADE}\n"
+                )
+
     def togglePotential(self):
         self.canvas.toggle_potential()
 
     def toggleLinks(self):
         self.canvas.toggle_links()
-    
+
     def load(self):
-        fname = self.fchooser("Open File") 
+        fname = self.fchooser("Open File")
         with open(fname, "r") as f:
             atoms = []
             for line in f.readlines():
-                x,y = line.split(",")
+                x, y = line.split(",")
                 atoms.append(Point(float(x), float(y)))
         self.canvas.load(atoms)
 
     def fchooser(self, message):
-        fname,_ = QFileDialog.getSaveFileName(
-                self,
-                message,
-                "./",
-                "(*.csv)",
-            )
-        return fname.split(".")[0]+".csv"
+        fname, _ = QFileDialog.getSaveFileName(
+            self,
+            message,
+            "./",
+            "(*.csv)",
+        )
+        return fname.split(".")[0] + ".csv"
 
     def clear(self):
         self.canvas.clear()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app = QApplication(sys.argv)
     app.setStyleSheet(open("../stylesheets/pyqt5-dark-theme.stylesheet").read())
     gui = CircuitGui()
